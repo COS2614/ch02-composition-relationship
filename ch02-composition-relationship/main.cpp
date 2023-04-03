89701867867f736bf59eb9bdf0f1c9a471815470
mitchcamza <192mitch88@gmail.com>
2023-04-03 18:02:04 +0200

    Add Person, Car, and Engine classes to demonstrate a composition relationship

--- ch02-composition-relationship/main.cpp
+++ ch02-composition-relationship/main.cpp
@@ -1,9 +1,41 @@
 #include <iostream>
 
-using namespace std;
+class Engine
+{
+public:
+    void start()
+    {
+        std::cout << "Engine started" << std::endl;
+    }
+};
+
+class Car
+{
+private:
+    Engine engine;
+public:
+    void start()
+    {
+        engine.start();
+        std::cout << "Car started" << std::endl;
+    }
+};
+
+class Person
+{
+private:
+    Car car;
+public:
+    void drive()
+    {
+        car.start();
+        std::cout << "Person is driving the car" << std::endl;
+    }
+};
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    Person person;
+    person.drive();
     return 0;
 }
